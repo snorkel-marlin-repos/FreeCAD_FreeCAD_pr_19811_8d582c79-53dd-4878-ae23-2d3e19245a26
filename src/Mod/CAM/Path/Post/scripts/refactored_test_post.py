1ac1a82a55512c0af2f3dfdd4e3dc8e644b26d1f
Adrian Insaurralde <adrianinsaval@gmail.com>
2025-02-25 23:56:49 -0300

    CMake: add missing ifs to support building 1.0 with LibPack 3.1.0

---
+++ src/Mod/CAM/Path/Post/scripts/refactored_test_post.py
@@ -0,0 +1,286 @@
+# ***************************************************************************
+# *   Copyright (c) 2014 sliptonic <shopinthewoods@gmail.com>               *
+# *   Copyright (c) 2022 - 2025 Larry Woestman <LarryWoestman2@gmail.com>   *
+# *   Copyright (c) 2024 Ondsel <development@ondsel.com>                    *
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import argparse
+
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from Path.Post.Processor import PostProcessor
+import Path.Post.UtilsArguments as PostUtilsArguments
+import Path.Post.UtilsExport as PostUtilsExport
+
+import Path
+import FreeCAD
+
+translate = FreeCAD.Qt.translate
+
+DEBUG = False
+if DEBUG:
+    Path.Log.setLevel(Path.Log.Level.DEBUG, Path.Log.thisModule())
+    Path.Log.trackModule(Path.Log.thisModule())
+else:
+    Path.Log.setLevel(Path.Log.Level.INFO, Path.Log.thisModule())
+
+#
+# Define some types that are used throughout this file.
+#
+Defaults = Dict[str, bool]
+FormatHelp = str
+GCodeOrNone = Optional[str]
+GCodeSections = List[Tuple[str, GCodeOrNone]]
+Parser = argparse.ArgumentParser
+ParserArgs = Union[None, str, argparse.Namespace]
+Postables = Union[List, List[Tuple[str, List]]]
+Section = Tuple[str, List]
+Sublist = List
+Units = str
+Values = Dict[str, Any]
+Visible = Dict[str, bool]
+
+
+class Refactored_Test(PostProcessor):
+    """The Refactored Test post processor class."""
+
+    def __init__(self, job) -> None:
+        super().__init__(
+            job=job,
+            tooltip=translate("CAM", "Refactored Test post processor"),
+            tooltipargs=[""],
+            units="Metric",
+        )
+        self.reinitialize()
+        Path.Log.debug("Refactored Test post processor initialized")
+
+    def reinitialize(self) -> None:
+        """Initialize or reinitialize the 'core' data structures for the postprocessor."""
+        #
+        # This is also used to reinitialize the data structures between tests.
+        #
+        self.values: Values = {}
+        self.init_values(self.values)
+        self.argument_defaults: Defaults = {}
+        self.init_argument_defaults(self.argument_defaults)
+        self.arguments_visible: Visible = {}
+        self.init_arguments_visible(self.arguments_visible)
+        self.parser: Parser = self.init_arguments(
+            self.values, self.argument_defaults, self.arguments_visible
+        )
+        #
+        # Create another parser just to get a list of all possible arguments
+        # that may be output using --output_all_arguments.
+        #
+        self.all_arguments_visible: Visible = {}
+        for k in iter(self.arguments_visible):
+            self.all_arguments_visible[k] = True
+        self.all_visible: Parser = self.init_arguments(
+            self.values, self.argument_defaults, self.all_arguments_visible
+        )
+
+    def init_values(self, values: Values) -> None:
+        """Initialize values that are used throughout the postprocessor."""
+        #
+        PostUtilsArguments.init_shared_values(values)
+        #
+        # Set any values here that need to override the default values set
+        # in the init_shared_values routine.
+        #
+        # Used in the argparser code as the "name" of the postprocessor program.
+        #
+        values["MACHINE_NAME"] = "test"
+        #
+        # Don't output comments by default.
+        #
+        values["OUTPUT_COMMENTS"] = False
+        #
+        # Don't output the header by default.
+        #
+        values["OUTPUT_HEADER"] = False
+        #
+        # Convert M56 tool change commands to comments,
+        # which are then suppressed by default.
+        #
+        values["OUTPUT_TOOL_CHANGE"] = False
+        values["POSTPROCESSOR_FILE_NAME"] = __name__
+        #
+        # Do not show the editor by default since we are testing.
+        #
+        values["SHOW_EDITOR"] = False
+        #
+        # Don't show the current machine units by default.
+        #
+        values["SHOW_MACHINE_UNITS"] = False
+        #
+        # Don't show the current operation label by default.
+        #
+        values["SHOW_OPERATION_LABELS"] = False
+        #
+        # Don't output an M5 command to stop the spindle after an M6 tool change by default.
+        #
+        values["STOP_SPINDLE_FOR_TOOL_CHANGE"] = False
+        #
+        # Don't output a G43 tool length command following tool changes by default.
+        #
+        values["USE_TLO"] = False
+        values["UNITS"] = self._units
+
+    def init_argument_defaults(self, argument_defaults: Defaults) -> None:
+        """Initialize which arguments (in a pair) are shown as the default argument."""
+        PostUtilsArguments.init_argument_defaults(argument_defaults)
+        #
+        # Modify which argument to show as the default in flag-type arguments here.
+        # If the value is True, the first argument will be shown as the default.
+        # If the value is False, the second argument will be shown as the default.
+        #
+        # For example, if you want to show Metric mode as the default, use:
+        #   argument_defaults["metric_inch"] = True
+        #
+        # If you want to show that "Don't pop up editor for writing output" is
+        # the default, use:
+        #   argument_defaults["show-editor"] = False.
+        #
+        # Note:  You also need to modify the corresponding entries in the "values" hash
+        #        to actually make the default value(s) change to match.
+        #
+
+    def init_arguments_visible(self, arguments_visible: Visible) -> None:
+        """Initialize which argument pairs are visible in TOOLTIP_ARGS."""
+        PostUtilsArguments.init_arguments_visible(arguments_visible)
+        #
+        # Modify the visibility of any arguments from the defaults here.
+        #
+        # Make all arguments invisible by default.
+        #
+        for key in iter(arguments_visible):
+            arguments_visible[key] = False
+
+    def init_arguments(
+        self,
+        values: Values,
+        argument_defaults: Defaults,
+        arguments_visible: Visible,
+    ) -> Parser:
+        """Initialize the shared argument definitions."""
+        _parser: Parser = PostUtilsArguments.init_shared_arguments(
+            values, argument_defaults, arguments_visible
+        )
+        #
+        # Add any argument definitions that are not shared with other postprocessors here.
+        #
+        return _parser
+
+    def process_arguments(self) -> Tuple[bool, ParserArgs]:
+        """Process any arguments to the postprocessor."""
+        #
+        # This function is separated out to make it easier to inherit from this postprocessor.
+        #
+        args: ParserArgs
+        flag: bool
+
+        (flag, args) = PostUtilsArguments.process_shared_arguments(
+            self.values, self.parser, self._job.PostProcessorArgs, self.all_visible, "-"
+        )
+        #
+        # If the flag is True, then all of the arguments should be processed normally.
+        #
+        if flag:
+            #
+            # Process any additional arguments here.
+            #
+            #
+            # Update any variables that might have been modified while processing the arguments.
+            #
+            self._units = self.values["UNITS"]
+        #
+        # If the flag is False, then args is either None (indicating an error while
+        # processing the arguments) or a string containing the argument list formatted
+        # for output.  Either way the calling routine will need to handle the args value.
+        #
+        return (flag, args)
+
+    def process_postables(self) -> GCodeSections:
+        """Postprocess the 'postables' in the job to g code sections."""
+        #
+        # This function is separated out to make it easier to inherit from this postprocessor.
+        #
+        gcode: GCodeOrNone
+        g_code_sections: GCodeSections
+        partname: str
+        postables: Postables
+        section: Section
+        sublist: Sublist
+
+        postables = self._buildPostList()
+
+        Path.Log.debug(f"postables count: {len(postables)}")
+
+        g_code_sections = []
+        for _, section in enumerate(postables):
+            partname, sublist = section
+            gcode = PostUtilsExport.export_common(self.values, sublist, "-")
+            g_code_sections.append((partname, gcode))
+
+        return g_code_sections
+
+    def export(self) -> Union[None, GCodeSections]:
+        """Process the parser arguments, then postprocess the 'postables'."""
+        args: ParserArgs
+        flag: bool
+
+        Path.Log.debug("Exporting the job")
+
+        (flag, args) = self.process_arguments()
+        #
+        # If the flag is True, then continue postprocessing the 'postables'.
+        #
+        if flag:
+            return self.process_postables()
+        #
+        # The flag is False meaning something unusual happened.
+        #
+        # If args is None then there was an error during argument processing.
+        #
+        if args is None:
+            return None
+        #
+        # Otherwise args will contain the argument list formatted for output
+        # instead of the "usual" gcode.
+        #
+        return [("allitems", args)]  # type: ignore
+
+    @property
+    def tooltip(self):
+        tooltip: str = """
+        This is a postprocessor file for the CAM workbench.  It is used
+        to test the postprocessor code.  It probably isn't useful for "real" gcode.
+        """
+        return tooltip
+
+    @property
+    def tooltipArgs(self) -> FormatHelp:
+        return self.parser.format_help()
+
+    @property
+    def units(self) -> Units:
+        return self._units
---
+++ src/Mod/Start/Gui/Resources/translations/StartPage.ts
@@ -0,0 +1,201 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>StartGui::FirstStartWidget</name>
+    <message>
+        <location filename="../../FirstStartWidget.cpp" line="95"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../FirstStartWidget.cpp" line="97"/>
+        <source>Welcome to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../FirstStartWidget.cpp" line="100"/>
+        <source>To get started, set your basic configuration options below.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../FirstStartWidget.cpp" line="101"/>
+        <source>These options (and many more) can be changed later in Preferences.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StartGui::GeneralSettingsWidget</name>
+    <message>
+        <location filename="../../GeneralSettingsWidget.cpp" line="212"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../GeneralSettingsWidget.cpp" line="213"/>
+        <source>Unit System</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../GeneralSettingsWidget.cpp" line="226"/>
+        <source>Navigation Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdStart</name>
+    <message>
+        <location filename="../../Manipulator.cpp" line="47"/>
+        <location filename="../../Manipulator.cpp" line="48"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Manipulator.cpp" line="49"/>
+        <source>Displays the Start in an MDI view</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StartGui::StartView</name>
+    <message>
+        <location filename="../../StartView.cpp" line="212"/>
+        <source>Empty file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="213"/>
+        <source>Create a new empty FreeCAD file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="215"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="216"/>
+        <source>Open an existing CAD file or 3D model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="218"/>
+        <source>Parametric Part</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="219"/>
+        <source>Create a part with the Part Design workbench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="221"/>
+        <source>Assembly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="222"/>
+        <source>Create an assembly project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="224"/>
+        <source>2D Draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="225"/>
+        <source>Create a 2D Draft with the Draft workbench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="227"/>
+        <source>BIM/Architecture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="228"/>
+        <source>Create an architectural project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="494"/>
+        <source>New File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="495"/>
+        <source>Examples</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="496"/>
+        <source>Recent Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="499"/>
+        <source>Open first start setup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../StartView.cpp" line="501"/>
+        <source>Don&apos;t show this Start page again (start with blank screen)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../../StartView.cpp" line="488"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StartGui::ThemeSelectorWidget</name>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="58"/>
+        <source>FreeCAD Classic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="59"/>
+        <source>FreeCAD Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="60"/>
+        <source>FreeCAD Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="166"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="167"/>
+        <source>Looking for more themes? You can obtain them using &lt;a href=&quot;freecad:Std_AddonMgr&quot;&gt;Addon Manager&lt;/a&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="169"/>
+        <source>FreeCAD Dark</source>
+        <comment>Visual theme name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="170"/>
+        <source>FreeCAD Light</source>
+        <comment>Visual theme name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../ThemeSelectorWidget.cpp" line="171"/>
+        <source>FreeCAD Classic</source>
+        <comment>Visual theme name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
